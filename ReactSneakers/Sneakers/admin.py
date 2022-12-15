807dfbdfefeeae7ebb761eba7309f5683da9facd
Milikin2 <nikitkagovoruhina@mail.ru>
2022-12-15 09:40:08 +0500

    Убрал модель работника

--- ReactSneakers/Sneakers/admin.py
+++ ReactSneakers/Sneakers/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from .models import Product
-from .models import Worker
 from .models import User
 from .models import Fav
 from .models import Bought
@@ -8,7 +7,6 @@ from .models import Bought
 # Register your models here.
 
 admin.site.register(Product)
-admin.site.register(Worker)
 admin.site.register(User)
 admin.site.register(Fav)
 admin.site.register(Bought)
